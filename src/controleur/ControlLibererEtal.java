fcd38bdf44c8ef0847c037e1cedc9bd89169c6f2
J3fr3Y <jefreysanchez888@gmail.com>
2025-03-26 17:10:36 +0100

    methode isVendeur dans classe ControlLibererEtal

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import villagegaulois.Etal;
+
 public class ControlLibererEtal {
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 
@@ -8,7 +10,10 @@ public class ControlLibererEtal {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
 
-	//TODO a completer
+	boolean isVendeur(String nomVendeur) {
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		return etal != null;
+	}
 
 	/**
 	 * 
