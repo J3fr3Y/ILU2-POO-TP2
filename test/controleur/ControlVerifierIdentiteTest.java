349b34d835d82bf6d058dbe82f5c2c971994401c
J3fr3Y <jefreysanchez888@gmail.com>
2025-04-03 16:45:33 +0200

    ajout ControlVeriferIdentiteTest

--- test/controleur/ControlVerifierIdentiteTest.java
+++ test/controleur/ControlVerifierIdentiteTest.java
@@ -2,13 +2,29 @@ package controleur;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ControlVerifierIdentiteTest {
+import personnages.Chef;
+import villagegaulois.Village;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+class ControlVerifierIdentiteTest {
+	private Village village;
+	private Chef abraracourcix;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("Le village des irréducibles",10,5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
 	}
 
+	@Test
+	void testVerifierIdentite() {
+		ControlVerifierIdentite controlVerifierIdentite = new ControlVerifierIdentite(village);
+		assertTrue(controlVerifierIdentite.verifierIdentite("Abraracourcix"));
+		assertFalse(controlVerifierIdentite.verifierIdentite("Pas habitant"));
+		
+	}
 }
